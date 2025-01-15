f1962a1c8b1b8264454760830f399de6ffc3ec7a
Louis Marcellino <30467303+marcelliino@users.noreply.github.com>
2025-01-15 11:41:34 +0700

    animating bar and logo

--- platinumcineplex/doodleFace/javascript/main_graphics.js
+++ platinumcineplex/doodleFace/javascript/main_graphics.js
@@ -1,28 +1,60 @@
 function draw() {
     background(86, 19, 187);
-    let minWin = min(width, height);
+    scene.minWin = min(width, height);
 
     if (file.loading) {
+        scene.logo.target = {
+            x: width / 2,
+            y: height / 2 - scene.minWin / 6,
+            s: scene.minWin / 2,
+        };
 
-        file.tracker.update(file.counter);
-
-        push();
-        tint(251, 128, 0);
-        imageMode(CENTER);
-        if (file.content.logo) image(file.content.logo, width / 2, height / 2 - minWin / 4, minWin / 2, minWin / 2);
-        pop();
-
-
-        file.tracker.display.bar(width / 2, height / 1.5, width / 2);
-
+        scene.bar.target = {
+            w: width / 2,
+            h: width / 32,
+            x: width / 2,
+            y: height / 1.5
+        };
     } else {
-        if (inout.webcam.prepared) inout.webcam.render(scene.graphic);
+        scene.logo.target.s = scene.minWin / 4;
+        scene.logo.target.x = scene.logo.target.s / 2;
+        scene.logo.target.y = scene.logo.target.s / 2;
+        scene.bar.target.x = width / 2;
+        scene.bar.target.y = height / 2;
+        scene.bar.target.w = width / 2;
+        scene.bar.target.h = width / 8;
     }
+
+    scene.logo.current = {
+        x: lerp(scene.logo.current.x, scene.logo.target.x, 0.125),
+        y: lerp(scene.logo.current.y, scene.logo.target.y, 0.125),
+        s: lerp(scene.logo.current.s, scene.logo.target.s, 0.125)
+    };
+
+    scene.bar.current = {
+        x: lerp(scene.bar.current.x, scene.bar.target.x, 0.125),
+        y: lerp(scene.bar.current.y, scene.bar.target.y, 0.125),
+        w: lerp(scene.bar.current.w, scene.bar.target.w, 0.125),
+        h: lerp(scene.bar.current.h, scene.bar.target.h, 0.125)
+    };
+
+
+    push();
+    tint(251, 128, 0);
+    imageMode(CENTER);
+    if (file.content.logo) image(file.content.logo, scene.logo.current.x, scene.logo.current.y, scene.logo.current.s, scene.logo.current.s, 0, 0);
+    pop();
+
+    file.tracker.update(file.counter);
+    file.tracker.display.bar(scene.bar.current.x, scene.bar.current.y, scene.bar.current.w, scene.bar.current.h);
+
+    if (inout.webcam.prepared) inout.webcam.render(scene.graphic);
 
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     inout.webcam.resize(width, height);
+
     scene.graphic.resizeCanvas(width, height);
 }
